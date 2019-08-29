55b4c95e63cb0aad02cc0ecafbbdcaab474bf1d5
jiangzilong <zilong4691@163.com>
2019-08-29 15:46:24 +0800

    优化

--- project/common/components/helpers/CacheHelper.php
+++ project/common/components/helpers/CacheHelper.php
@@ -14,7 +14,7 @@ class CacheHelper extends BaseHelper
     /**
      * 缓存时长 ：1天
      */
-    const CACHE_1_DAY = 86400;
+    const CACHE_ONE_DAY = 86400;
 
     /**
      * 缓存时长 ： 1小时
@@ -24,7 +24,7 @@ class CacheHelper extends BaseHelper
     /**
      * 缓存时长 ：1分钟
      */
-    const CACHE_TEN_ONE_MINUTE = 60;
+    const CACHE_ONE_MINUTE = 60;
 
     //-------------------------------------缓存时长基数,自定义时长以基数计算-------------------------------------//
 }
