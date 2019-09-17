fd7216b7ff83d9d6908c37e19359bc40559f3cbb
jiangzilong <zilong4691@163.com>
2019-09-17 11:31:11 +0800

    优化

--- project/common/service/ExceptionService.php
+++ project/common/service/ExceptionService.php
@@ -30,14 +30,18 @@ class ExceptionService extends BaseService
         if($result)
         {
 //            MailService::instance()->send('jiangzilong@zhibo.tv','出错啦','你看这错出的','直播TV哦');
-            MailService::instance()->tplSend('jiangzilong@zhibo.tv','网站错误报告【'.date('Y-m-d H:i:s').'】','error',[
-                'file' => $exception->getFile(),
-                'code' => $exception->getCode(),
-                'line' => $exception->getLine(),
-                'id' => $appId,
-                'message' => $exception->getMessage(),
-                'trace' => $exception->getTraceAsString()
-            ],'中国体育');
+            try{
+                MailService::instance()->tplSend('jiangzilong@zhibo.tv','网站错误报告【'.date('Y-m-d H:i:s').'】','error',[
+                    'file' => $exception->getFile(),
+                    'code' => $exception->getCode(),
+                    'line' => $exception->getLine(),
+                    'id' => $appId,
+                    'message' => $exception->getMessage(),
+                    'trace' => $exception->getTraceAsString()
+                ],'中国体育');
+            }catch (\Exception $e){
+                ExceptionErrorLogEntity::model()->recordError($exception->getCode(),$exception->getFile(),$exception->getLine(),$exception->getMessage(),$exception->getTraceAsString(),$appId,$serverInfo);
+            }
         }
     }
 }
