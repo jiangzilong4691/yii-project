6cb95c3b248b8241da3e5d3dd2aab741bf1259cb
jiangzilong <zilong4691@163.com>
2020-06-02 17:44:49 +0800

    更新

--- project/common/base/BaseController.php
+++ project/common/base/BaseController.php
@@ -10,54 +10,59 @@ use yii\web\Response;
 class BaseController extends Controller
 {
     /**
-     * 接口json数据返回 无特殊要求不再直接exit输出
+     * api json 接口数据返回
      *
-     * @param $returnInfo
-     *
-     * @return \yii\web\Response
-     *
-     * @Author: 姜子龙 <jiangzilong@zhibo.tv>
-     * @Date: 2019/8/5
-     * @Time: 15:17
-     */
-    protected function apiJson($returnInfo=[],$code = '200',$msg = 'success')
-    {
-        if(isset($returnInfo['code'],$returnInfo['desc']))
-        {
-            $code = $returnInfo['code'];
-            $msg = $returnInfo['desc'];
-            $returnInfo = [];
-        }
-        return $this->asJson([
-            'status' => $code,
-            'msg' => $msg,
-            'data' => $returnInfo
-        ]);
-    }
-
-    /**
-     * 跨域json数据返回
-     *
-     * @param   string      $callBack       回调函数名
-     * @param   array       $data           返回信息
-     * @param   string      $status         返回状态码
-     * @param   string      $msg            返回状态信息
+     * @param mixed     $returnData     接口返回数据
+     * @param string    $status         返回状态码
+     * @param string    $msg            状态信息
      *
      * @return \yii\console\Response|Response
      *
      * @Author: 姜子龙 <jiangzilong@zhibo.tv>
      * @Date: 2020/3/23
-     * @Time: 17:34
+     * @Time: 16:54
+     */
+    protected function apiJson($returnData,$status='200',$msg='success')
+    {
+        $response = \Yii::$app->getResponse();
+        $response->format = Response::FORMAT_JSON;
+        if(is_array($returnData) && isset($returnData['code'],$returnData['desc']))
+        {
+            $status = $returnData['code'];
+            $msg    = $returnData['desc'];
+            $returnData = isset($returnData['data'])?$returnData['data']:[];
+        }
+        $response->data = [
+            'status' => $status,
+            'msg' => $msg,
+            'data' => $returnData
+        ];
+        return $response;
+    }
+
+    /**
+     * api jsonp jsonp跨域接口数据返回
+     *
+     * @param   string      $callBack   跨域函数名值
+     * @param   mixed       $data       返回数据信息
+     * @param   string      $status     返回状态码
+     * @param   string      $msg        返回状态信息
+     *
+     * @return \yii\console\Response|Response
+     *
+     * @Author: 姜子龙 <jiangzilong@zhibo.tv>
+     * @Date: 2020/3/23
+     * @Time: 17:03
      */
     protected function apiJsonp($callBack,$data,$status='200',$msg='success')
     {
         $response = \Yii::$app->getResponse();
         $response->format = Response::FORMAT_JSONP;
-        if(isset($returnData['code'],$returnData['desc']))
+        if(is_array($data) && isset($returnData['code'],$returnData['desc']))
         {
             $status = $data['code'];
             $msg    = $data['desc'];
-            $returnData = [];
+            $returnData = isset($data['data'])?$data['data']:[];
         }
         $response->data = [
             'callback' => $callBack,
